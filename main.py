ff92ac1f64b6054334fbcf43ef7be7b06cb9cda4
Nitehawk <nitehawk@nitespace.co>
2025-09-08 02:54:31 +0000

    Count characters

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from stats import count_words
+from stats import count_chars
 
 def get_book_text(book):
     with open(book) as f:
@@ -10,7 +11,9 @@ def get_book_text(book):
 def main():
     book = get_book_text("books/frankenstein.txt")
     count = count_words(book)
+    cchar = count_chars(book)
     print(f"{count} words found in the document")
+    print(cchar)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_chars(text):
+    lstr = text.lower()
+    ccount = {}
+    for c in lstr:
+        if c in ccount:
+            ccount[c] += 1
+        else:
+            ccount[c] = 1
+
+    return ccount
+
