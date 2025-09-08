5cfc155d278bbe24f041a0228e00f840be352951
Nitehawk <nitehawk@nitespace.co>
2025-09-08 02:48:29 +0000

    Split stats

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
+from stats import count_words
+
 def get_book_text(book):
     with open(book) as f:
         text = f.read()
     return text
-
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 def main():
     book = get_book_text("books/frankenstein.txt")
