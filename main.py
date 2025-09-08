b7e76140c762ff976ce4bd22b74e908e5c0563f0
Nitehawk <nitehawk@nitespace.co>
2025-09-08 02:40:27 +0000

    Read a file.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+#!/usr/bin/python3
+
+def get_book_text(book):
+    with open(book) as f:
+        text = f.read()
+    print(text)
+
+def main():
+    get_book_text("books/frankenstein.txt")
+
+
+main()
+
